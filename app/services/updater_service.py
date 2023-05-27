0547b05d4c29049e2bd90a095049032dcd6f0f83
qodize <vladislav.gusev.dev@gmail.com>
2023-05-27 22:08:23 +0300

    updater_service

---
+++ app/services/updater_service.py
@@ -0,0 +1,29 @@
+import asyncio
+import random
+
+from app.dto.update_transport import UpdateTransport
+from app.repos.abstract.locations_repository_interface import LocationsRepositoryInterface
+from app.repos.abstract.transports_repository_interface import TransportsRepositoryInterface
+
+
+class UpdaterService:
+    def __init__(
+            self,
+            transport_repo: TransportsRepositoryInterface,
+            locations_repo: LocationsRepositoryInterface
+    ):
+        self.transport_repo = transport_repo
+        self.locations_repo = locations_repo
+
+    async def start_updating(self):
+        while True:
+            await asyncio.sleep(3 * 60)
+            transports = await self.transport_repo.get_transports()
+            locations = await self.locations_repo.get_locations()
+            for transport in transports:
+                await self.transport_repo.update_transport(
+                    transport.transport_id,
+                    UpdateTransport(
+                        location_zipcode=random.choice(locations).zipcode
+                    )
+                )
