fbead96f730a423a93a202d17b845504d9e457e4
qodize <vladislav.gusev.dev@gmail.com>
2023-05-27 21:38:41 +0300

    create -id

--- app/dto/create_shipment.py
+++ app/dto/create_shipment.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class CreateShipment(BaseModel):
-    id: int
     pick_up_zipcode: str
     delivery_zipcode: str
     weight: int
--- app/dto/create_transport.py
+++ app/dto/create_transport.py
@@ -2,7 +2,5 @@ from pydantic import BaseModel
 
 
 class CreateTransport(BaseModel):
-    transport_id: int
     number: str
-    current_location_zipcode: str
     max_weight: int
