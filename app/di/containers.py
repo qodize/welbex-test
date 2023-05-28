ed9fa06a416c1d0616a5bc3037d9dbf71c56d8f5
qodize <vladislav.gusev.dev@gmail.com>
2023-05-28 03:27:58 +0300

    Container finish, main include routers and start updating

--- app/di/containers.py
+++ app/di/containers.py
@@ -3,6 +3,12 @@ import os
 from dependency_injector import containers, providers
 
 from app.db.database import Database
+from app.repos.impl.locations_repository import LocationsRepository
+from app.repos.impl.shipments_repository import ShipmentsRepository
+from app.repos.impl.transports_repository import TransportsRepository
+from app.services.shipments_service import ShipmentsService
+from app.services.transports_service import TransportsService
+from app.services.updater_service import UpdaterService
 
 
 class Container(containers.DeclarativeContainer):
@@ -11,4 +17,35 @@ class Container(containers.DeclarativeContainer):
     db = providers.Singleton(
         Database,
         f'postgresql+asyncpg://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_SERVER"]}/{os.environ["POSTGRES_DB"]}'
+    )
+
+    locations_repository = providers.Factory(
+        LocationsRepository,
+        session_factory=db.provided.session
+    )
+
+    transports_repository = providers.Factory(
+        TransportsRepository,
+        session_factory=db.provided.session
+    )
+
+    shipments_repository = providers.Factory(
+        ShipmentsRepository,
+        session_factory=db.provided.session
+    )
+
+    transports_service = providers.Factory(
+        TransportsService,
+        transports_repository.provided
+    )
+
+    shipments_service = providers.Factory(
+        ShipmentsService,
+        shipments_repository.provided
+    )
+
+    updater_service = providers.Singleton(
+        UpdaterService,
+        transports_repository.provided,
+        locations_repository.provided
     )
--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
+import asyncio
+
 from fastapi import FastAPI
 
 from app.di.containers import Container
+from app.routers import shipments_router, transports_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    app.include_router(shipments_router.router)
+    app.include_router(transports_router.router)
+
     container = Container()
     app.container = container
 
     @app.on_event('startup')
     async def startup():
         await app.container.db().init_models()
+        asyncio.create_task(app.container.updater_service().start_updating())
 
     return app
 
